c6566f364b74545d9bf94869fffa9c240688c431
S Kunath <skunath@gmail.com>
2013-03-03 22:03:19 -0500

    Started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdoremusScheduler::Application.config.secret_token = '67661d893682c35bd14235f4fb97838d69136a10cc9d70d5f82f213ac7badf4018235f8be7fdc164245584c53e33964f8981230b4ef54db6dc70c5447224d9d1'
